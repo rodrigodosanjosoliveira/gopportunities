f280a558e7c199b0b6558d9dd346fe78f90f8659
Rodrigo Oliveira <rodrigodosanjosoliveira@gmail.com>
2023-12-09 16:53:59 -0300

    feat: add router package

--- main.go
+++ main.go
@@ -1,13 +1,8 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import "github.com/rodrigodosanjosoliveira/gopportunities/router"
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	r.Run()
+	// Initialize Router
+	router.Initialize()
 }
